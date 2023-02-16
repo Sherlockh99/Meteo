b5189b61de6913f2b82a59efd9c074883282b0c4
Sherlock <sherlockh989@gmail.com>
2023-02-16 20:05:58 +0200

    Added MainViewModel.kt

--- app/src/main/java/com/sherlock/gb/kotlin/meteo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sherlock/gb/kotlin/meteo/ui/main/MainViewModel.kt
@@ -1,7 +1,26 @@
 package com.sherlock.gb.kotlin.meteo.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sherlock.gb.kotlin.meteo.repository.RepositoryImpl
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val liveData : MutableLiveData<AppState> = MutableLiveData(),
+    private val repository : RepositoryImpl = RepositoryImpl()
+) : ViewModel() {
+    fun getData() = liveData
+
+    fun getWeather(){
+        Thread{
+            liveData.postValue(AppState.Loading)
+
+            if((0..10).random()>5) {
+                val answer = repository.getWeatherFromServer()
+                //TODO HW параметр с переключателем - локально или сервер
+                liveData.postValue(AppState.Success(answer))
+            }else{
+                liveData.postValue(AppState.Error(IllegalAccessException()))
+            }
+        }.start()
+    }
 }
